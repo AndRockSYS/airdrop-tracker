f0fbb9ee6bedffa1512828e9896a960e5e448fbe
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-31 22:08:58 +0300

    added navbar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import dynamic from 'next/dynamic';
+
+const WalletButton = dynamic(() => import('@/components/WalletButton'), { ssr: false });
+
+import './nav.css';
+
+export default function Nav() {
+    return (
+        <nav>
+            <h2>Airdrop Tracker</h2>
+            <WalletButton />
+        </nav>
+    );
+}
