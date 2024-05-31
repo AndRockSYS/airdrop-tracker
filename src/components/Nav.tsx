a1bd755678beb63a1b0c1a568571fbcf9716ab47
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-31 23:20:55 +0300

    added add airdrop button to the navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 
 const WalletButton = dynamic(() => import('@/components/WalletButton'), { ssr: false });
@@ -5,9 +7,17 @@ const WalletButton = dynamic(() => import('@/components/WalletButton'), { ssr: f
 import './nav.css';
 
 export default function Nav() {
+    const showAirdropForm = () => {
+        const form = document.querySelector('section.add-airdrop') as HTMLElement;
+        form.style.display = 'block';
+    };
+
     return (
         <nav>
             <h2>Airdrop Tracker</h2>
+            <button id='button' className='add-airdrop' onClick={showAirdropForm}>
+                Add Airdrop
+            </button>
             <WalletButton />
         </nav>
     );
