283451fc77ff34ab2b692246caadbb8ecbc3dc8c
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-31 17:02:49 +0300

    added wallet connection

---
+++ src/context/Web3Modal.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react';
+
+const projectId = 'YOUR_PROJECT_ID';
+
+const mainnet = {
+    chainId: 1,
+    name: 'Ethereum',
+    currency: 'ETH',
+    explorerUrl: 'https://etherscan.io',
+    rpcUrl: 'https://cloudflare-eth.com',
+};
+
+const metadata = {
+    name: 'My Website',
+    description: 'My Website description',
+    url: 'https://mywebsite.com',
+    icons: ['https://avatars.mywebsite.com/'],
+};
+
+const ethersConfig = defaultConfig({
+    metadata,
+    defaultChainId: 1,
+});
+
+createWeb3Modal({
+    ethersConfig,
+    chains: [mainnet],
+    projectId,
+    enableAnalytics: true,
+    enableOnramp: true,
+});
+
+export function Web3Modal({ children }: { children: React.ReactNode }) {
+    return children;
+}
