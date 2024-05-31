98cf93290a2986caa6375db628e1f01982353d60
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-05-31 22:08:32 +0300

    added types for everything

--- typings.ts
+++ typings.ts
@@ -1,13 +1,47 @@
 export type Airdrop = {
     name: string;
-    tier: Tier;
+    progress: Progress;
+    tags: string; //!many
+
+    costToFarm: string; //!many
+    chainTech: string; //!many
+    stage: string;
+
+    tier: string;
+    priority: string;
+    status: string;
+    funding?: number;
+    val?: number;
+
+    bridgeDateAt?: number;
+    firstTxAt?: number;
+
+    completion: string;
+
+    referralURL?: string;
+
+    //relatedProjects
 
     createdAt: number;
     editedAt: number;
 };
 
+export type Progress =
+    | 'Not Started'
+    | 'Planning'
+    | 'Unconfirmed'
+    | 'In Progress'
+    | 'Farming'
+    | 'Waiting for Announcement'
+    | 'Announced'
+    | 'Canceled'
+    | 'Ended'
+    | 'Claimed'
+    | 'Archieve'
+    | 'Snapshot Taken';
+
+export type Tier = 'S' | 'A' | 'B' | 'C' | 'D' | 'F';
+
 export type Tag = {
     name: string;
 };
-
-export type Tier = 'S' | 'A' | 'B' | 'C' | 'D' | 'F';
