5014b440906d34d5e3dcfc28cfa62b996a083067
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-02 18:23:37 +0300

    chnaged row structure

--- src/components/AirdropRow.tsx
+++ src/components/AirdropRow.tsx
@@ -1,42 +1,59 @@
 import { convertTimestampToDate, stringToTags } from '@/utils/utils';
 
-import { Airdrop } from 'types';
+import Glacier from '@/service/Glacier';
 
-import tagStyle from './tag.module.css';
+import { Airdrop, Tag } from 'types';
 
-export default function AirdropRow(airdrop: Airdrop) {
+export default function AirdropRow(
+    airdrop: Airdrop,
+    isOwner: boolean,
+    glacier: Glacier | undefined,
+    tags: { [key: string]: Tag } | undefined
+) {
+    const tagsToObjects = (input: string | undefined): JSX.Element[] => {
+        return stringToTags(input).map((tagName) => {
+            return (
+                <div
+                    id='tag'
+                    key={tagName}
+                    style={{ backgroundColor: tags && tags[tagName] ? tags[tagName].color : '' }}
+                >
+                    {tagName}
+                </div>
+            );
+        });
+    };
     return (
         <tr key={airdrop.name}>
             <td>{airdrop.name}</td>
             <td>{airdrop.tier}</td>
-            <td>
-                {stringToTags(airdrop.costToFarm).map((item) => (
-                    <div key={item.name} className={tagStyle.tag}>
-                        {item.name}
-                    </div>
-                ))}
-            </td>
+            <td>{tagsToObjects(airdrop.costToFarm)}</td>
             <td>{airdrop.status}</td>
             <td>{airdrop.progress}</td>
             <td>{airdrop.funding ? `$${airdrop.funding}` : ''}</td>
             <td>{airdrop.val ? `$${airdrop.val}` : ''}</td>
             <td>{airdrop.stage}</td>
-            <td>
-                {stringToTags(airdrop.tags).map((item) => (
-                    <div key={item.name} className={tagStyle.tag}>
-                        {item.name}
-                    </div>
-                ))}
-            </td>
-            <td>
-                {stringToTags(airdrop.chainTech).map((item) => (
-                    <div key={item.name} className={tagStyle.tag}>
-                        {item.name}
-                    </div>
-                ))}
-            </td>
+
+            <td>{tagsToObjects(airdrop.tags)}</td>
+            <td>{tagsToObjects(airdrop.chainTech)}</td>
+
             <td>{convertTimestampToDate(airdrop.createdAt)}</td>
             <td>{convertTimestampToDate(airdrop.editedAt)}</td>
+            {isOwner ? (
+                <td>
+                    <button id='button'>Edit</button>
+                    <button
+                        id='button'
+                        onClick={() => {
+                            if (glacier) glacier.deleteAirdrop(airdrop.name);
+                        }}
+                    >
+                        Delete
+                    </button>
+                </td>
+            ) : (
+                <></>
+            )}
         </tr>
     );
 }
