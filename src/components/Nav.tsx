7ae0b9609c90f44defc6bad91a18eff99d7d803f
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-05 12:43:07 +0300

    updates to new hooks

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,23 +4,24 @@ import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 
 import useOwner from '@/hooks/useOwner';
+import useForm from '@/hooks/useForm';
+
 const WalletButton = dynamic(() => import('@/components/WalletButton'), { ssr: false });
+
+import { FormState } from 'types';
 
 import './nav.css';
 
 export default function Nav() {
     const { isOwner } = useOwner();
-    const [addButton, setAddButton] = useState(<></>);
+    const { openForm } = useForm();
 
-    const showAirdropForm = () => {
-        const form = document.querySelector('section.airdrop-form') as HTMLElement;
-        form.style.display = 'block';
-    };
+    const [addButton, setAddButton] = useState(<></>);
 
     useEffect(() => {
         if (isOwner)
             setAddButton(
-                <button id='button' className='add-airdrop' onClick={showAirdropForm}>
+                <button id='button' className='add-airdrop' onClick={() => openForm(FormState.Add)}>
                     Add Airdrop
                 </button>
             );
