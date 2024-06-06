fd0ae4ebcda212e2b06748af280baf79b2132529
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-06 13:38:12 +0300

    remake the msg to new state

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,14 +1,21 @@
 'use client';
 
-import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers/react';
+import { useDisconnect, useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers/react';
+import { useMemo } from 'react';
 
 export default function WalletButton() {
     const { open } = useWeb3Modal();
+    const { disconnect } = useDisconnect();
     const { address } = useWeb3ModalAccount();
 
+    const message = useMemo(
+        () => (address ? address.slice(0, 5) + '...' + address.slice(38, 42) : 'Connect Wallet'),
+        [address]
+    );
+
     return (
-        <button id='button' onClick={() => open()}>
-            {address ? address.slice(0, 5) + '...' + address.slice(38, 42) : 'Connect Wallet'}
+        <button id='button' onClick={() => (message == 'Connect Wallet' ? open() : disconnect())}>
+            {message}
         </button>
     );
 }
