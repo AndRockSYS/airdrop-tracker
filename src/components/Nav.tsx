d611ed0da70ea45f7412c6caac60b8490b447c65
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-07 00:54:23 +0300

    added link to social medias

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 
 const WalletButton = dynamic(() => import('@/components/WalletButton'), { ssr: false });
 
@@ -10,6 +11,10 @@ export default function Nav() {
     return (
         <nav>
             <h2>Airdrop Tracker</h2>
+            <div>
+                <Image src={'/telegram.png'} alt='telegram' width={25} height={25}></Image>
+                <Image src={'/x.png'} alt='x' width={25} height={25}></Image>
+            </div>
             <WalletButton />
         </nav>
     );
