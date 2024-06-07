e9e84a4578d789313a7ffa5e5963b2b2daa690cd
Andris <106549955+AndRockSYS@users.noreply.github.com>
2024-06-07 13:24:27 +0300

    added link for tg

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,19 +6,23 @@ import Image from 'next/image';
 const WalletButton = dynamic(() => import('@/components/WalletButton'), { ssr: false });
 
 import './nav.css';
+import Link from 'next/link';
 
 export default function Nav() {
     return (
         <nav>
             <h2>Airdrop Tracker</h2>
             <div>
-                <Image
-                    id='images'
-                    src={'/telegram.png'}
-                    alt='telegram'
-                    width={25}
-                    height={25}
-                ></Image>
+                <Link href={'https://telegram.org'}>
+                    <Image
+                        id='images'
+                        src={'/telegram.png'}
+                        alt='telegram'
+                        width={25}
+                        height={25}
+                    ></Image>
+                </Link>
+
                 <Image id='images' src={'/x.png'} alt='x' width={25} height={25}></Image>
                 <WalletButton />
             </div>
